6288148e8a3f2b1abc1a237d231656b70d02b712
keuwey <73517606+keuwey@users.noreply.github.com>
2021-09-01 14:11:41 -0300

    Add files via upload

---
+++ fatorial.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8; -*-
+# Este é um simples script para calcular o fatorial
+# de um número real e inteiro
+
+fat = int(input("Digite um número: "))
+f = 1
+for x in range(fat, f, -1):
+  f *= x
+
+print(f)
