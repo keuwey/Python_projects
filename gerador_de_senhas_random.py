021a2db1d9cce5ccbb16fe00d1fb7f4aa7189d67
keuwey <73517606+keuwey@users.noreply.github.com>
2021-09-01 16:55:52 -0300

    Update gerador_de_senhas_random.py

--- gerador_de_senhas_random.py
+++ gerador_de_senhas_random.py
@@ -1,5 +1,10 @@
+# -*- coding: utf-8; -*-
+# Este script é um gerador de senhas que trabalha usando a
+# biblioteca de números pseudoaleatórios e nele é possível definir
+# o tamanho da senha
+
 import random
-caracteres = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '_', '+', '£', '¢', '§', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '/', '?', '°', '[', '{', '~', '^', ']', 'º', ',', '.', ';', '<', '>', ':', '|', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z']
+caracteres = [' ', '!', '@', '#', '$', '%', '&', '*', '(', ')', '_', '+', '£', '¢', '§', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '/', '?', '°', '[', '{', '~', '^', ']', 'º', ',', '.', ';', '<', '>', ':', '|', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z']
 tam_senha = int(input("Qual o tamanho da senha: "))
 print("\n")
 random.shuffle(caracteres)
