f5af96a4a52e48597a74cdd883dd2dcb4a4eed15
keuwey <73517606+keuwey@users.noreply.github.com>
2021-09-04 18:11:24 -0300

    Create numero_primo.py

---
+++ numero_primo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8; -*-
+# Um script para checar se um dado número é primo
+# Por definição, um número primo é todo número N inteiro
+# que é divisível apenas por 1 e por ele mesmo
+# Ex.: 3, 5, 7, 11 etc
+
+numero = int(input("Digite um número: "))
+n = numero - 1
+if (n ** numero) % numero == n:
+    print("\n%d é primo" % numero)
+else:
+    print("\n%d não é primo" % numero)
