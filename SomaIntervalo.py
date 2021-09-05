b44eec7f5ccff1551f6fa80daa112f8d8c2f5718
keuwey <73517606+keuwey@users.noreply.github.com>
2021-09-04 21:07:02 -0300

    Update SomaIntervalo.py

--- SomaIntervalo.py
+++ SomaIntervalo.py
@@ -3,9 +3,12 @@
 # Ex.: 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
 
 def somaAte(limite):
-  soma = 0
-  numero = 1
-  while numero <= limite:
-    soma += numero
-    numero += 1
-  return soma
+    soma = 0
+    numero = 1
+    while numero <= limite:
+        soma += numero
+        numero += 1
+    return "\n" + soma
+    
+if __name__ == '__main__':
+    print(somaAte(int(input("Digite um número: "))))
