05545b69c2823d6307fc2343a208e485e42d15e0
keuwey <73517606+keuwey@users.noreply.github.com>
2021-09-08 02:47:02 -0300

    Update bit_flipper.py

--- bit_flipper.py
+++ bit_flipper.py
@@ -1,5 +1,9 @@
-# Um script de manipulação de bits, trocando 0s por 1s
-# Ex.: >>> 0100101
-# .... 1011010
+"""
+
+This is a simple bit manipulation script, swapping 0s for 1s
+Ex.: >>> 0100101
+.... 1011010
+
+"""
 
 print(''.join('1' if x == '0' else '0' for x in input()))
