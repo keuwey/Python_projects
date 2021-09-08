965ae3e2d49e8c7d9584306c4763c0ccf169f46d
keuwey <73517606+keuwey@users.noreply.github.com>
2021-09-08 02:44:45 -0300

    Update bhaskara.py

--- bhaskara.py
+++ bhaskara.py
@@ -1,13 +1,21 @@
-# -*- coding: utf-8; -*-
-# Esse script resolve equações de segundo grau utilizando a famosa
-# fórmula de Bháskara
+"""
 
-a = int(input("Digite a: "))
-b = int(input("Digite b: "))
-c = int(input("Digite c: "))
+This script solves quadratic equations using the
+Bhaskara's formula
+
+"""
+
+print()
+a = float(input("Digite a: "))
+b = float(input("Digite b: "))
+c = float(input("Digite c: "))
 delta = (b**2) - 4 * a * c
 if a != 0:
     x1 = (-b + (delta ** (1/2))) / (2 * a)
     x2 = (-b - (delta ** (1/2))) / (2 * a)
+else:
+    print("a precisa ser diferente de zero (a ≠ 0)")
 
-print("As raízes são x' = ", x1, " e x'' = ", x2)
+print()
+print("As raízes são x' = %.2f e x'' = %.2f" % (x1, x2))
+print()
