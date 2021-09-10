c73ec33c02abec281f43fbdb94cce41a7ad70a44
keuwey <73517606+keuwey@users.noreply.github.com>
2021-09-10 01:59:20 -0300

    Update coversor_de_tempo.py

--- coversor_de_tempo.py
+++ coversor_de_tempo.py
@@ -1,14 +1,13 @@
-# coding: -*- utf-8; -*-
 print("\n")
-print("-" * 50)
+print("-" * 60)
 print("\nBem vindo(a)! Escolha uma das operações abaixo:\n")
-print("Converter horas em segundos: 1")
-print("Converter horas em minutos: 2")
-print("Converter minutos em horas: 3")
-print("Converter minutos em segundos: 4")
-print("Converter segundos em horas: 5")
-print("Converter segundos para minutos: 6\n")
-print("-" * 50)
+print("Converter horas em segundos:\t\t\t1")
+print("Converter horas em minutos:\t\t\t2")
+print("Converter minutos em horas:\t\t\t3")
+print("Converter minutos em segundos:\t\t\t4")
+print("Converter segundos em horas:\t\t\t5")
+print("Converter segundos em minutos:\t\t\t6\n")
+print("-" * 60)
 print("\n")
 while True:
     operacao = input("\nDigite o número da operação ou Q para sair: ")
@@ -24,8 +23,10 @@ while True:
         print("\n%.2f horas é igual a %.2f minutos" % (tempo, resultado))
     elif operacao == "3":
         tempo = float(input("Digite os minutos: "))
-        resultado = tempo / 60
-        print("\n%.2f minutos é igual a %.2f hora(s)" % (tempo, resultado))
+        resultado = tempo // 60
+        minutos = tempo % 60
+        # segundos = (tempo // 60) % 60
+        print("\n%.2f minutos é igual a %.0fh %.0fm" % (tempo, resultado, minutos))
     elif operacao == "4":
         tempo = float(input("Digite os minutos: "))
         resultado = tempo * 60
@@ -33,7 +34,9 @@ while True:
     elif operacao == "5":
         tempo = float(input("Digite os segundos: "))
         resultado = (tempo / 60) / 60
-        print("\n%.2f segundos é igual a %.2f hora(s)" % (tempo, resultado))
+        minutos = (tempo / 60) % 60
+        # segundos = ((tempo / 60) /60) % 60
+        print(f"\n{tempo:.2f} segundos é igual a {resultado:.2f} hora(s) e {minutos:.2f} minutos") # e {segundos:.2f} segundos")
     elif operacao == "6":
         tempo = float(input("Digite os segundos: "))
         resultado = tempo / 60
