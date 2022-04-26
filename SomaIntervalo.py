2c07d1ce7fd59b9fc40bf011950a3afacf95938e
keuwey <73517606+keuwey@users.noreply.github.com>
2022-04-25 22:12:39 -0300

    Update SomaIntervalo.py

--- SomaIntervalo.py
+++ SomaIntervalo.py
@@ -1,6 +1,6 @@
 """
 
-This script sums the natural numbers up to a given limit.
+Esse programa soma os números inteiros naturais até um dado limite.
 Ex.: 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
 
 """
