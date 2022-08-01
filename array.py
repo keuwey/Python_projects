4f34e992aba8950b61b652c88019d1ea65fbf377
keuwey <coemgein600673@gmail.com>
2022-08-01 16:41:02 -0300

    Consegui fazer funcionar de duas formas diferentes, mas tive que pedir ajuda no stackoverflow lol

--- array.py
+++ array.py
@@ -1,7 +1,12 @@
-numbers = [5,9,3,19,70,8,100,2,35,27]
+# numbers = [5,9,3,19,70,8,100,2,35,27,33,0,1,21]
+# out = [a * b for a, b in zip(numbers, numbers[1:] + [2])]
+# print(out)
+
+numbers = [5,9,3,19,70,8,100,2,35,27,33,0,1,21]
 new_array = []
 x = 0
-while x <= 8:
-    new_array.append(numbers[x] * numbers[x + 1])
+while x < len(numbers):
+    nxt = 2 if x+1 >= len(numbers) else numbers[x+1]
+    new_array.append(numbers[x] * nxt)
     x += 1
 print(new_array)
